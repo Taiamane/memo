5dc6bde168ad45a95d7fac6d8fadaf8b6f00fcf0
Taiamane <fujiura-taishu749@g.ecc.u-tokyo.ac.jp>
2025-06-07 16:32:11 +0900

    ページ遷移のやり方まで復習完了

--- my-app-ts/src/App.tsx
+++ my-app-ts/src/App.tsx
@@ -1,15 +1,23 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter, Route, Routes, Link } from "react-router-dom";
+import Howtouse from './components/Howtouse';
 
 function App() {
   return (
     <div className="App">
+      <BrowserRouter>
+        <Routes>
+          <Route path='/howtouse' Component={Howtouse} />
+        </Routes>  
+      
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           これをメモ帳アプリの雛形とする！！
         </p>
+          <Link to='/howtouse'>使い方のページはこちら</Link>
         <a
           className="App-link"
           href="https://reactjs.org"
@@ -19,6 +27,7 @@ function App() {
           Learn React
         </a>
       </header>
+      </BrowserRouter>
     </div>
   );
 }
---
+++ my-app-ts/src/components/Howtouse.tsx
@@ -0,0 +1,14 @@
+import React from 'react'; // Reactをインポート
+
+
+function Howtouse(){
+  // --- JSX (UIの記述) ---
+  return (
+    <div>      
+      <p>
+        使い方のページだよ
+      </p>
+    </div>
+  );
+};
+export default Howtouse;
