ce3b8977a54ef911f55df626b4156f8010ade88c
Taiamane <fujiura-taishu749@g.ecc.u-tokyo.ac.jp>
2025-06-21 10:45:21 +0900

    投稿機能コード完了

--- my-app-ts/src/components/Postnewmemo.tsx
+++ my-app-ts/src/components/Postnewmemo.tsx
@@ -4,12 +4,56 @@ import { User } from 'firebase/auth'; // FirebaseのUser型をインポート
 
 interface NewmemoProps {
   currentUser: User; // ログインユーザーの情報を必ず受け取るので User 型
-  apiEndpoint: string; // APIエンドポイントも必要なので受け取る
+  apiEndpoint: string; // 投稿先のDBに関するエンドポイントを置く
 }
 
 const PostnewMemo: React.FC<NewmemoProps> = ({ currentUser, apiEndpoint }) => {
   const[memocontent, setMemoContent] = useState<string>("何か入力してね");
   const[memotitle, setMemoTitle] = useState<string>("");
+
+  const handlePostSubmit = async () => {
+    if (!memotitle){
+      setMemoTitle("無題")
+    }
+    if (!memocontent){
+      setMemoContent("No content")
+    }
+    // ロケールを指定して日付を取得するサンプルコード
+  const now = new Date();
+  const options: Intl.DateTimeFormatOptions = {
+    year: 'numeric',
+    month: 'long',
+    day: 'numeric',
+    hour: '2-digit',
+    minute: '2-digit',
+    second: '2-digit'
+  };
+  const madeDate = now.toLocaleDateString('ja-JP', options);
+  const updatedDate = now.toLocaleDateString('ja-JP', options);
+
+  try{
+    const response = await fetch(apiEndpoint, {
+      method:'POST',
+      headers:{
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+          user: currentUser,
+          madeDate: madeDate,
+          updatedDate: updatedDate,
+          title: memotitle,
+          content:memocontent,
+        }),
+    });
+    if (!response.ok) {
+        const errorData = await response.json();
+        throw new Error(errorData.message || '投稿失敗');
+      }
+      
+  }catch (error:any){
+    console.error('POSTエラー:',error);
+    }
+  };
 
     return (
       <div>
@@ -21,7 +65,7 @@ const PostnewMemo: React.FC<NewmemoProps> = ({ currentUser, apiEndpoint }) => {
             type="text"
             value={memotitle}
             onChange={(e) => setMemoTitle(e.target.value)}
-            placeholder="IDを入力"
+            placeholder="タイトルを入力"
           />
           <label htmlFor="memo-content">内容：</label>
           <input
@@ -32,7 +76,10 @@ const PostnewMemo: React.FC<NewmemoProps> = ({ currentUser, apiEndpoint }) => {
             placeholder="内容を入力"
           />
           </div>  
-      </div>  )
+          <button onClick={handlePostSubmit}>投稿する</button>
+      </div>
+      
+    )
 
 
 }
